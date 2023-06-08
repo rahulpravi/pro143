7b7f8eae551fd3a1fac4bc193ce83ff9f3077233
Rahul@666 <40337774+rahulpravi@users.noreply.github.com>
2023-06-08 16:58:55 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -92,18 +92,4 @@
         });
     }, {offset: '80%'});
 
-
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
-
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
-
-    
 })(jQuery);
